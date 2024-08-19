fcd8242841e7b0dfd347149c1953a76498e1a5d7
Harsha vardhan <harshavardhan1617@gmail.com>
2024-08-19 21:16:26 +0530

    initial list component complete

--- src/cardList/CardList.jsx
+++ src/cardList/CardList.jsx
@@ -1,15 +1,43 @@
 import Card from "../card/Card";
 import "./CardList.css";
 
-export default function CardList() {
+export default function CardList({ header }) {
   return (
-    <div className="list">
-      <Card
-        todo={"this is a test todo to check the state of the item totodododo"}
-      />
-      <Card
-        todo={"this is a test todo to check the state of the item totodododo"}
-      />
+    <div className="whole-card">
+      <h3> {header} </h3>
+      <div className="list">
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+        <Card
+          todo={"this is a test todo to check the state of the item totodododo"}
+        />
+      </div>
+      <div className="add-btn">
+        <span className="material-symbols-outlined">add</span>
+        <p>Add a Todo</p>
+      </div>
     </div>
   );
 }
