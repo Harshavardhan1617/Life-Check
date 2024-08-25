c95f456017253e50e07f22b8b2cfded0e322fd50
Harsha vardhan <harshavardhan1617@gmail.com>
2024-08-25 21:26:21 +0530

    add adding todos for cardList UI

--- src/cardList/CardList.jsx
+++ src/cardList/CardList.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import React, { useRef } from "react";
+
 import Card from "../card/Card";
 import "./CardList.css";
 
 export default function CardList({ header }) {
+  var [isBeingAdded, setAdd] = useState(false);
+
+  const showEdit = () => {
+    setAdd((isBeingAdded = !isBeingAdded));
+  };
   return (
     <div className="whole-card">
       <h3> {header} </h3>
@@ -33,8 +41,13 @@ export default function CardList({ header }) {
         <Card
           todo={"this is a test todo to check the state of the item totodododo"}
         />
+        {isBeingAdded && (
+          <div className="add-input">
+            <input type="text" />
+          </div>
+        )}
       </div>
-      <div className="add-btn">
+      <div className="add-btn" onClick={showEdit}>
         <span className="material-symbols-outlined">add</span>
         <p>Add a Todo</p>
       </div>
