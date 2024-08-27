a6a40371f5785ec31cbae866a4df37246b348e83
Harsha vardhan <harshavardhan1617@gmail.com>
2024-08-27 11:12:04 +0530

    chore: stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-import Card from "./card/Card";
+import CardList from "./cardList/CardList";
+import Life from "./life/Life";
+import Week from "./week/Week";
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
-      <Card
-        todo={
-          "this is a test todo and a whole lotaa more todos are coming right up s a test todo and a whole lotaa more todos are coming right"
-        }
-      />
+
+      <Life />
     </>
   );
 }
