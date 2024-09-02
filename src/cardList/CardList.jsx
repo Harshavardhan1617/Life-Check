c387e9f9e6e15e532eab9647fcacd4d9cc891d8e
Harsha vardhan <harshavardhan1617@gmail.com>
2024-09-02 19:30:05 +0530

    feat: add append todos feature

--- src/cardList/CardList.jsx
+++ src/cardList/CardList.jsx
@@ -5,31 +5,25 @@ import Card from "../card/Card";
 import "./CardList.css";
 import InputTodo from "../InputTodo/InputTodo";
 
-export default function CardList({ todo }) {
+export default function CardList({ todo, appendTodos, timeStamp }) {
   var [isBeingAdded, setAdd] = useState(false);
-  const [addTodo, setNewTodo] = useState("");
-
   const showEdit = () => {
     setAdd((isBeingAdded = !isBeingAdded));
   };
 
-  const updateTodo = (evt) => {
-    setNewTodo(evt.target.value);
-  };
   return (
     <div className="whole-card">
       <div className="list">
         {todo.map((el) => {
           return <Card todo={el.text} key={el.id} />;
         })}
-
-        {isBeingAdded && (
-          <InputTodo addTodo={addTodo} updateTodo={updateTodo} />
-        )}
       </div>
+      {isBeingAdded && <InputTodo appendTodos={appendTodos} />}
       <div className="add-btn" onClick={showEdit}>
-        <span className="material-symbols-outlined">add</span>
-        <p>Add a Todo</p>
+        {!isBeingAdded && (
+          <span className="material-symbols-outlined">add</span>
+        )}
+        {isBeingAdded ? <p>Done</p> : <p>Add a Todo</p>}
       </div>
     </div>
   );
