60c5cfc1e4a902df0c820945a7292bb90e1e427a
Harsha vardhan <harshavardhan1617@gmail.com>
2024-09-02 19:32:37 +0530

    fix: append todos

--- src/InputTodo/InputTodo.jsx
+++ src/InputTodo/InputTodo.jsx
@@ -1,9 +1,22 @@
 import "./InputTodo.css";
+import { useState } from "react";
 
-export default function InputTodo({ addTodo, updateTodo }) {
+export default function InputTodo({ appendTodos }) {
+  const [addTodo, setNewTodo] = useState("");
+  const updateTodo = (evt) => {
+    setNewTodo(evt.target.value);
+  };
   return (
     <div className="add-input">
       <textarea value={addTodo} onChange={updateTodo}></textarea>
+      <button
+        onClick={() => {
+          appendTodos(addTodo);
+          setNewTodo("");
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
