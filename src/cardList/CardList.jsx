fc0481b5171b70f1f9fbc3594b172637c2840e4e
Harsha vardhan <harshavardhan1617@gmail.com>
2024-09-02 19:33:55 +0530

    add calendar function to label todos

--- src/cardList/CardList.jsx
+++ src/cardList/CardList.jsx
@@ -5,14 +5,34 @@ import Card from "../card/Card";
 import "./CardList.css";
 import InputTodo from "../InputTodo/InputTodo";
 
+const calendarData = (stamp) => {
+  const result = {
+    weekNumber: "",
+    month: "",
+    year: "",
+  };
+  const date = new Date(stamp);
+
+  const weekNumber = Math.ceil(date.getDate() / 7);
+  const weekLabels = ["first", "second", "third", "fourth", "fifth"];
+  result.weekNumber = weekLabels[weekNumber - 1] || "other";
+
+  result.month = date.toLocaleString("en-US", { month: "long" });
+  result.year = date.getFullYear();
+
+  return result;
+};
+
 export default function CardList({ todo, appendTodos, timeStamp }) {
   var [isBeingAdded, setAdd] = useState(false);
   const showEdit = () => {
     setAdd((isBeingAdded = !isBeingAdded));
   };
-
+  const timeStampData = calendarData(timeStamp);
   return (
     <div className="whole-card">
+      <p>{`${timeStampData.weekNumber} week of ${timeStampData.month}, ${timeStampData.year}`}</p>
+
       <div className="list">
         {todo.map((el) => {
           return <Card todo={el.text} key={el.id} />;
