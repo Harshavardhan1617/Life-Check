53698ee7626ef892517c5c1988139a1e83c063a5
Harsha Vardhan <harshavardhan1617@gmail.com>
2024-10-25 22:50:51 +0530

    fix: append and delete todos malfunction

--- src/week/Week.jsx
+++ src/week/Week.jsx
@@ -38,51 +38,25 @@ export default function Week({ todos: initialTodos, timeStamp }) {
     setAnchorEl(null);
   }, []);
 
-  const appendTodos = useCallback(
-    async (text) => {
-      const newTodoID = crypto.randomUUID();
-      const newTodo = { todoID: newTodoID, text, isChecked: false };
+  const appendTodos = async (text) => {
+    const newTodoID = crypto.randomUUID();
+    const newTodo = { todoID: newTodoID, text: text, isChecked: false };
 
-      try {
-        setTodos((prevTodos) => [...prevTodos, newTodo]);
-        await Promise.all([
-          db.todos.add(newTodo),
-          db.weeks
-            .where("timeStamp")
-            .equals(timeStamp)
-            .modify((week) => {
-              week.todo = [...week.todo, newTodoID];
-            }),
-        ]);
-      } catch (error) {
-        setTodos((prevTodos) =>
-          prevTodos.filter((todo) => todo.todoID !== newTodoID)
-        );
-        console.error("Failed to add todo:", error);
-      }
-    },
-    [timeStamp]
-  );
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
 
-  const deleteTodos = useCallback(
-    async (id) => {
-      try {
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.todoID !== id));
-        await Promise.all([
-          db.todos.delete(id),
-          db.weeks
-            .where("timeStamp")
-            .equals(timeStamp)
-            .modify((week) => {
-              week.todo = week.todo.filter((todoId) => todoId !== id);
-            }),
-        ]);
-      } catch (error) {
-        console.error("Failed to delete todo:", error);
-      }
-    },
-    [timeStamp]
-  );
+    await db.todos.add(newTodo);
+    const week = await db.weeks.get(timeStamp);
+    await db.weeks.put({ ...week, todo: [...week.todo, newTodoID] });
+  };
+
+  const deleteTodos = async (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.todoID !== id));
+
+    await db.todos.delete(id);
+    const week = await db.weeks.get(timeStamp);
+    const updatedTodos = week.todo.filter((todoId) => todoId !== id);
+    await db.weeks.put({ ...week, todo: updatedTodos });
+  };
 
   return (
     <div className="week-wrap">
